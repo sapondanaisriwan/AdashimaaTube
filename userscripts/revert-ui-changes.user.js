d904d7638ae250ce58b928091e48c0d763e6e94b
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2024-04-12 11:51:26 +0700

    update revert ui changes

--- userscripts/revert-ui-changes.user.js
+++ userscripts/revert-ui-changes.user.js
@@ -10,6 +10,7 @@
 // @icon           https://i.imgur.com/I9uDrsq.png
 // @run-at         document-start
 // @homepageURL    https://github.com/sapondanaisriwan/AdashimaaTube
+// @downloadURL    https://github.com/sapondanaisriwan/AdashimaaTube/raw/master/userscripts/revert-ui-changes.user.js
 // @updateURL      https://github.com/sapondanaisriwan/AdashimaaTube/raw/master/userscripts/revert-ui-changes.user.js
 // @supportURL     https://github.com/sapondanaisriwan/AdashimaaTube/issues
 // ==/UserScript==
@@ -29,37 +30,37 @@ Support me: https://ko-fi.com/sapondanaisriwan
 
 // Define the flags to assign to the EXPERIMENT_FLAGS object
 const flagsToAssign = {
-    kevlar_watch_grid: false,
-    kevlar_watch_grid_hide_chips: false,
+  kevlar_watch_grid: false,
+  kevlar_watch_grid_hide_chips: false,
 
-    // fix the old description data is missing
-    kevlar_watch_metadata_refresh_no_old_secondary_data: false,
+  // fix the old description data is missing
+  kevlar_watch_metadata_refresh_no_old_secondary_data: false,
 
-    enable_channel_page_header_profile_section: false,
-    kevlar_modern_sd_v2: false,
+  enable_channel_page_header_profile_section: false,
+  kevlar_modern_sd_v2: false,
 
-    kevlar_watch_comments_panel_button: false,
-    smartimation_background: false,
+  kevlar_watch_comments_panel_button: false,
+  smartimation_background: false,
 
-    web_animated_actions: false,
-    web_modern_collections_v2: false,
-    // https://i.imgur.com/DEl82Nt.png
-    web_modern_tabs: false,
-    web_enable_youtab: false,
+  web_animated_actions: false,
+  web_modern_collections_v2: false,
+  // https://i.imgur.com/DEl82Nt.png
+  web_modern_tabs: false,
+  web_enable_youtab: false,
 
-    // fix the full screen icon overlapse: https://i.imgur.com/ZGZZVeW.png
-    kevlar_set_internal_player_size: false,
+  // fix the full screen icon overlapse: https://i.imgur.com/ZGZZVeW.png
+  kevlar_set_internal_player_size: false,
 };
 
 const updateFlags = () => {
-    // Check if the EXPERIMENT_FLAGS object exists in the window.yt.config_ property chain
-    const expFlags = window?.yt?.config_?.EXPERIMENT_FLAGS;
+  // Check if the EXPERIMENT_FLAGS object exists in the window.yt.config_ property chain
+  const expFlags = window?.yt?.config_?.EXPERIMENT_FLAGS;
 
-    // If EXPERIMENT_FLAGS is not found, exit the function
-    if (!expFlags) return;
+  // If EXPERIMENT_FLAGS is not found, exit the function
+  if (!expFlags) return;
 
-    // Assign the defined flags to the EXPERIMENT_FLAGS object
-    Object.assign(expFlags, flagsToAssign);
+  // Assign the defined flags to the EXPERIMENT_FLAGS object
+  Object.assign(expFlags, flagsToAssign);
 };
 
 // Create a MutationObserver that calls the updateFlags function when changes occur in the document's subtree
